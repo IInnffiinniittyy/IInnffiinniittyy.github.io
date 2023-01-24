926c13a9d4072b84bc1f7b42ce785a6c9d389534
IInnffiinniittyy <tanuj813@gmail.com>
2023-01-24 20:16:43 +0530

    submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,32 @@
 /* =============== My Learnings Model =============== */
-const modalViews = document.querySelectorAll('.services__modal'),
-      modalBtns = document.querySelectorAll('.services__button'),
-      modalClose = document.querySelectorAll('.services__modal-close')
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalClose = document.querySelectorAll(".services__modal-close");
 
-let modal = function(modalClick) {
-    modalViews[modalClick].classList.add('active-modal')
-}
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
 
 modalBtns.forEach((mb, i) => {
-    mb.addEventListener('click', () => {
-        modal(i)
-    })
-})
+  mb.addEventListener("click", () => {
+    modal(i);
+  });
+});
 
 modalClose.forEach((mc) => {
-    mc.addEventListener('click', () => {
-        modalViews.forEach((mv) => {
-            mv.classList.remove('active-modal')
-        })
-    })
-})
+  mc.addEventListener("click", () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove("active-modal");
+    });
+  });
+});
+
+let button = document.getElementById("download");
+button.addEventListener("click", () => {
+  window.open("./assets/pdf/Tanuj-Pant-Resume.pdf");
+});
+
+let button1 = document.getElementById("resume");
+button1.addEventListener("click", () => {
+  window.open("./assets/pdf/Tanuj-Pant-Resume.pdf");
+});
